e6f0be2f7b6413404c50cfd4d3eb0291e709d041
soruprohan <sorupr5@gmail.com>
2026-03-09 17:04:56 +0600

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/marketplace/mini_marketplace/MiniMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.mini_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarketplaceApplication.class, args);
+	}
+
+}
